ddcc8266fd1c9ea1291d87840970a8772e98b3d5
Andrew Hinga <andrewhinga5@gmail.com>
2019-03-06 11:05:42 +0300

    Create a react app from scratch

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: __dirname,
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    },
+    resolve:{
+        extensions:["*",".js",".jsx"]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+             template: './src/index.html'
+        })
+    ]
+}
